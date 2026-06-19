e607aa905c3b3236b0eac188fa372eb33c6bc28b
Miruthula Rajendran <miruthumailing@gmail.com>
2026-06-19 13:52:01 +0530

    Update database configuration

--- library.py
+++ library.py
@@ -7,7 +7,7 @@ from datetime import date, timedelta
 load_dotenv()
 
 db = mysql.connector.connect(
-    host=os.getenv("DB_HOST",),
+    host=os.getenv("DB_HOST"),
     user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASSWORD"),
     database=os.getenv("DB_NAME")
